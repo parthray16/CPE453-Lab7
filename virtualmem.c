8114fb57f688d646613366ffb69476002de4c5f3
Parth Ray <parthray16@gmail.com>
2021-11-14 22:43:18 -0800

    gets page and offset

--- virtualmem.c
+++ virtualmem.c
@@ -3,5 +3,30 @@
 #include <string.h>
 
 int main(int argc, char* argv[]){
-    return 1;
+    unsigned int logicalAddy = 0;
+    unsigned int phyicalAddy = 0;
+    char value = 0;
+    unsigned int offsetMask = (1 << 8) - 1;
+    unsigned int pageMask = (1 << 16) - 1;
+    unsigned int pageNumber = 0;
+    unsigned int offsetNumber = 0;
+    FILE *addyFile = fopen(argv[1], "r");
+    
+    if (argc != 2){
+        fprintf(stderr, "usage: ./virtualmem addresses.txt\n");
+        return 1;
+    }
+
+    while(fscanf(addyFile, "%u", &logicalAddy) == 1){
+        pageNumber = (logicalAddy & pageMask) >> 8;
+        offsetNumber = logicalAddy & offsetMask;
+
+
+        fprintf(stdout, "Virtual address: %u Physical address: %u Value: %d", logicalAddy, phyicalAddy, value);
+    }
+
+    fclose(addyFile);
+
+
+    return 0;
 }
